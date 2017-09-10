47be9ba97b2e7ec015e69ee87374b85a06b6c325
gksaksdma1 <gksaksdma1@naver.com>
2017-09-10 23:49:55 +0900

    Initial commit

---
+++ app/src/main/java/com/example/joon/grid/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.joon.grid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    SingerAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GridView gridView = (GridView) findViewById(R.id.gridView);
+
+        adapter = new SingerAdapter();
+        adapter.addItem(new SingerItem("소녀시대","010-1000-1000",R.drawable.icon01));
+        adapter.addItem(new SingerItem("걸스데이","010-1000-2000",R.drawable.icon03));
+        adapter.addItem(new SingerItem("여자친구","010-1000-3000",R.drawable.icon04));
+        adapter.addItem(new SingerItem("오마이걸","010-1000-4000",R.drawable.icon05));
+        adapter.addItem(new SingerItem("레드벨벳","010-1000-5000",R.drawable.icon06));
+
+        gridView.setAdapter(adapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                SingerItem item = (SingerItem) adapter.getItem(position);
+                Toast.makeText(getApplicationContext(),"선택 : "+item.getName(),Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    class SingerAdapter extends BaseAdapter {
+        ArrayList<SingerItem> items = new ArrayList<SingerItem>();
+
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+
+        public void addItem(SingerItem item) {
+            items.add(item);
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            SingerItemView view = null;
+            if(convertView == null) {
+                view = new SingerItemView(getApplicationContext());
+            } else {
+                view =(SingerItemView) convertView;
+            }
+
+            SingerItem item = items.get(position);
+            view.setName(item.getName());
+            view.setMoible(item.getMobile());
+            view.setImage(item.getResId());
+
+            return view;
+        }
+
+
+    }
+}
